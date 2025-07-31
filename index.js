50649bc50c0780c6c99d3b783f80b60d532995a3
Walid Esse <walid-esse@Walids-Air.lan>
2025-07-31 18:39:30 -0500

    added project post

---
+++ index.js
@@ -0,0 +1,8 @@
+/* async function loadIndexPosts() {
+    
+    //this will load posts as either blogs or 'quick' posts
+
+    try {
+        const quickResponse = await fetch('https://walidesse/')
+    }
+} */
